c802a63e18c1f4fe407694948d28081d632ac5bc
GianbaBotassi <gianba89@gmail.com>
2023-11-20 15:15:46 +0100

    Add dependency and entity Utente and Ruolo, import into db

---
+++ src/main/java/com/experis/exercise/springLaMiaPizzeriaCrud/model/Ruolo.java
@@ -0,0 +1,28 @@
+package com.experis.exercise.springLaMiaPizzeriaCrud.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ruoli")
+public class Ruolo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/experis/exercise/springLaMiaPizzeriaCrud/model/Utente.java
@@ -0,0 +1,94 @@
+package com.experis.exercise.springLaMiaPizzeriaCrud.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "utenti")
+public class Utente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+    @Column(name = "registered_at")
+    private LocalDateTime registeredAt;
+
+    @ManyToMany
+    @JoinTable(name = "utenti_ruoli",
+            joinColumns = @JoinColumn(name = "utente_id"),
+            inverseJoinColumns = @JoinColumn(name = "ruolo_id"))
+    private Set<Ruolo> ruoli = new HashSet<>();
+
+    public Set<Ruolo> getRuoli() {
+        return ruoli;
+    }
+
+    public void setRuoli(Set<Ruolo> ruoli) {
+        this.ruoli = ruoli;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return email;
+    }
+
+    public void setMail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getRegisteredAt() {
+        return registeredAt;
+    }
+
+    public void setRegisteredAt(LocalDateTime registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+}
